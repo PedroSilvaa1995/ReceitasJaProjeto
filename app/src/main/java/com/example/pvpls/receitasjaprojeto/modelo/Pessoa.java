1d0fdf7525091fa386e604cd79b2beef94f82e50
Pedro Vincícius <pvpls@hotmail.com>
2017-11-30 01:09:55 -0200

    modelo classes

---
+++ app/src/main/java/com/example/pvpls/receitasjaprojeto/modelo/Pessoa.java
@@ -0,0 +1,14 @@
+package com.example.pvpls.receitasjaprojeto.modelo;
+
+/**
+ * Created by pvpls on 30/11/2017.
+ */
+
+public class Pessoa {
+    private int cd_Pessoa;
+    private String email;
+    private String senha;
+    private String descricao;
+    private String nome;
+    private String foto;
+}
---
+++ app/src/main/java/com/example/pvpls/receitasjaprojeto/modelo/Receita.java
@@ -0,0 +1,15 @@
+package com.example.pvpls.receitasjaprojeto.modelo;
+
+/**
+ * Created by pvpls on 30/11/2017.
+ */
+
+public class Receita {
+  public int cd_receita;
+  public String categoria;
+  public String foto_receita;
+  public String descricao;
+  public String SubCategoria;
+  public String receita;
+  public int Avaliacao;
+}
